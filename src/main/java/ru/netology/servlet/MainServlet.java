b053f84ac56d5784943f2476bf379d1edce64e01
Konstantin Shebalin <championacolironni@gmail.com>
2021-07-28 23:58:26 +0700

    Done"

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -18,9 +18,7 @@ public class MainServlet extends HttpServlet {
     @Override
     public void init() {
         final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
-        final var controller = context.getBean("postController");
-        final var service = context.getBean(PostService.class);
-        final var isSame = service == context.getBean("postService");
+        final var controller = context.getBean(PostController.class);
     }
 
     @Override
